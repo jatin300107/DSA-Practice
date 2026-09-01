d84faec613221e905730de0f03a4ff1e760dc355
jatin300107 <jatin30jan2007@gmail.com>
2026-09-02 00:39:36 +0530

    O(nlogn) version

--- Russian_Doll_Envelopes.py
+++ Russian_Doll_Envelopes.py
@@ -1,7 +1,7 @@
 from typing import List
 
 envelopes = [[4,5],[4,6],[6,7],[2,3],[1,1]]
-
+'''
 # O(n2) Approach 
 def maxEnvelopes( envelopes: List[List[int]]) -> int:
         n = len(envelopes)
@@ -13,5 +13,33 @@ def maxEnvelopes( envelopes: List[List[int]]) -> int:
                     dp[i] = max(dp[i],dp[j] +1)
 
         print(max(dp))
+
+maxEnvelopes(envelopes=envelopes) '''
+
+def search(x,tails):
+        
+        left = 0
+        right = len(tails)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if tails[mid] < x:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+def maxEnvelopes( envelopes: List[List[int]]) -> int:
+        n = len(envelopes)
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        tails = []
+        for i in  range(n):
+            x = envelopes[i][1]
+            left = search(x,tails)
+            if left == len(tails):
+                tails.append(x)
+            else:
+                tails[left] = x
+        print(len(tails))
 
 maxEnvelopes(envelopes=envelopes)
